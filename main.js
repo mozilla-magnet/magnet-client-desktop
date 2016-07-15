86a17382c283fc54b80fc699ccc4fc1129ad5e45
Wilson Page <wilsonpage@me.com>
2016-07-15 11:24:42 +0100

    Add drag-drop text to advertise URL

--- main.js
+++ main.js
@@ -5,10 +5,10 @@
  */
 
 const MagnetScanner = require('./lib/magnet-scanner');
+const advertise = require('./lib/advertiser');
 const debug = require('debug')('magnet:App');
 const electron = require('electron');
 const path = require('path');
-
 
 const {
   Menu,
@@ -34,7 +34,8 @@ function App() {
 App.prototype = {
   createTray() {
     const tray = new Tray(path.resolve('./IconTemplate.png'));
-    tray.setToolTip('This is my application');
+    tray.on('drop-text', this.onDropText.bind(this));
+    tray.setToolTip('Magnet');
     return tray;
   },
 
@@ -58,6 +59,19 @@ App.prototype = {
       }));
     }
 
+    if (this.ad) {
+      menu.append(new MenuItem({ type: 'separator' }));
+      menu.append(new MenuItem({
+        label: truncate(this.ad.url, 32),
+        enabled: false
+      }));
+
+      menu.append(new MenuItem({
+        label: 'Stop broadcasting',
+        click: this.stopAdvertising.bind(this)
+      }));
+    }
+
     menu.append(new MenuItem({ type: 'separator' }));
     menu.append(new MenuItem({
       label: 'Quit',
@@ -78,6 +92,22 @@ App.prototype = {
     debug('item lost', item);
     delete this.items[item.url];
     this.render();
+  },
+
+  onDropText(e, text) {
+    this.advertise(text);
+  },
+
+  advertise(url) {
+    if (!isUrl(url)) return;
+    this.ad = advertise(url);
+    this.render();
+  },
+
+  stopAdvertising() {
+    this.ad.stop();
+    delete this.ad;
+    this.render();
   }
 }
 
@@ -86,3 +116,16 @@ app.on('ready', () => {
     new App();
   } catch(err) { console.log('err', err);}
 });
+
+/**
+ * Utils
+ */
+
+function isUrl(string) {
+  return /^https?\:\/\//.test(string);
+}
+
+function truncate(string, max) {
+  if (string.length < max) return string;
+  return string.slice(0, max) + '\u2026';
+}
